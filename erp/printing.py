cd5fb04d5b3d6c0b0e7a562ae740739ce3d2c0c9
Ruud <ruud@habing.net>
2016-07-19 16:15:04 +0200

    reportlab template added table

--- erp/printing.py
+++ erp/printing.py
@@ -1,33 +1,14 @@
 from reportlab.lib.pagesizes import letter, A4
-from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_CENTER
+from reportlab.lib import colors
 from erp.models import serviceReport
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm, inch
 
 
 
-class NumberedCanvas(canvas.Canvas):
-    def __init__(self, *args, **kwargs):
-        canvas.Canvas.__init__(self, *args, **kwargs)
-        self._saved_page_states = []
-
-    def showPage(self):
-        self._saved_page_states.append(dict(self.__dict__))
-        self._startPage()
-
-    def save(self):
-        num_pages = len(self._saved_page_states)
-
-        for state in self._saved_page_states:
-            self.__dict__.update(state)
-            self.draw_page_number(num_pages)
-        canvas.Canvas.save(self)
-
-    def draw_page_number(self, page_count):
-        self.drawRightString( 211 * mm, 15 * mm + (0.2 * inch),
-            "Page %d of %d" % (self._pageNumber, page_count))
 
 
 
@@ -41,23 +22,7 @@ class MyPrint:
             self.pagesize = letter
             self.width, self.height = self.pagesize
 
-    @staticmethod
-    def _header_footer(canvas, doc):
-        canvas.saveState()
-        styles = getSampleStyleSheet()
-
-        # header
-
-        header = Paragraph('this is a multiline header.' * 5, styles['Normal'])
-        w, h = header.wrap(doc.width, doc.topMargin)
-        header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin + doc.bottomMargin - 2*h)
-
-        # footer 
-        footer = Paragraph('this is a multiline footer.' * 5, styles['Normal'])
-        w, h = footer.wrap(doc.width, doc.bottomMargin)
-        footer.drawOn(canvas, doc.leftMargin, h)
-
-        canvas.restoreState()
+    
 
 
     def export_report(self):
@@ -87,9 +52,20 @@ class MyPrint:
 
 
         elements.append(Paragraph('Vessel information', styles['Heading2']))
-        elements.append(Paragraph('Vessel name: '+report.seal.vessel.name, styles['Normal']))
-        elements.append(Paragraph('IMO: '+report.seal.vessel.imo, styles['Normal']))
-        elements.append(Paragraph('Company: '+report.seal.company.name, styles['Normal']))
+
+        data= [['Vessel', report.seal.vessel.name],
+           ['Company', report.seal.company.name],
+           ['IMO', report.seal.vessel.imo]]
+
+
+        t=Table(data, hAlign='LEFT') #,2*[3*inch], 3*[0.4*inch])
+        t.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+                       ('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+                       ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                       ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                       ]))
+
+        elements.append(t)
 
 
         elements.append(Paragraph('Contact information', styles['Heading2']))
@@ -101,7 +77,7 @@ class MyPrint:
 
 
 
-        doc.build(elements, onFirstPage = self._header_footer, onLaterPages = self._header_footer, canvasmaker=NumberedCanvas)
+        doc.build(elements)
 
         pdf = buffer.getvalue()
         buffer.close()
