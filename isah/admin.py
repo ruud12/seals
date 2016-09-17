390d0317b80b5c9126fe8174a25bdc958976726a
ruud habing <ruud@habing.net>
2016-09-17 14:55:43 +0200

    added admin register for sealpart and sealsize

--- isah/admin.py
+++ isah/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from isah.models import SealCompany
+from isah.models import SealCompany, SealSize, SealPart
 
 # Register your models here.
 
 admin.site.register(SealCompany)
+admin.site.register(SealSize)
+admin.site.register(SealPart)
 
 
 
